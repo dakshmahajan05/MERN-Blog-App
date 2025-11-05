01b87ca4a9a03a672b9feffdf73002b9b6c52d38
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-11-05 18:44:09 +0530

    display blogs on homepage

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { blogCategories } from '../Assets/assets.js'
+import { blog_data, blogCategories } from '../Assets/assets.js'
 import {motion} from 'framer-motion'
+import BlogCard from './BlogCard.jsx'
 
 const BlogList = () => {
     const [menu,setmenu] = useState('All')
@@ -8,19 +9,31 @@ const BlogList = () => {
     <div>
         <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
             {blogCategories.map((item)=>(
-                <div className='relative ' key={item}>
-                    <button onClick={()=>setmenu(item)} className={`cursor-pointer text-gray-500 ${menu===item && `text-white px-4 py-0.5`}`}>
-                        {item}
-                        {menu ===item && (
-                            <motion.div layoutId='underline' transition={{type:"spring",stiffness:500,damping:30}} className='absolute left-0 top-0 right-0 h-7 -z-1 bg-primary rounded-full '>
-                            </motion.div>
-                        )}
-                    </button>
-                </div>
+                <button 
+                    key={item}
+                    onClick={()=>setmenu(item)} 
+                    className={`relative cursor-pointer px-4 py-1 rounded-full transition 
+                    ${menu===item ? 'text-white' : 'text-gray-500'}`}
+                >
+                    {item}
+
+                    {menu === item && (
+                        <motion.div
+                            layoutId='underline'
+                            transition={{ type:"spring", stiffness:500, damping:30 }}
+                            className='absolute inset-0 bg-primary rounded-full -z-10'
+                        />
+                    )}
+                </button>
             ))}
         </div>
-        <div>
 
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mx-8 sm:mx-16 xl:mx-40'>
+            {blog_data
+              .filter((blog)=> menu==='All' ? true : blog.category===menu)
+              .map((blog)=> (
+                <BlogCard key={blog._id} blog={blog}/>
+            ))}
         </div>
     </div>
   )
