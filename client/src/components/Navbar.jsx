4606de5728e877c4ce2ab770987dd9a42f96fd9c
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-11-05 13:53:23 +0530

    header ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     const navigate = useNavigate()
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32  '>
-        <img src={assets.logo} alt="logo" className='w-32 sm:w-44 ' />
+        <img onClick={()=>navigate('/admin')} src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' />
         <button className='cursor-pointer flex items-center gap-2 rounded-full text-sm bg-primary px-10 text-white py-2.5'>Login 
-            <img src={assets.arrow} alt="" className='w-3' />
+            <img onClick={()=>navigate('/login')}  src={assets.arrow} alt="" className='w-3' />
         </button>
     </div>
   )
